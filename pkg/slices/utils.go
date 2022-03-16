13419538ded5e4d1cb9074e0fe8f70954ca2dbda
Marc-Antoine Argenton <maargenton.dev@gmail.com>
2022-03-15 22:09:59 -0700

    Fix 1 comment

--- pkg/slices/utils.go
+++ pkg/slices/utils.go
@@ -1,7 +1,7 @@
 package slices
 
 // Make is an helper function that creates a slice from individual elements
-// using type inference to determine the type of the resulting slice. ALl input
+// using type inference to determine the type of the resulting slice. All input
 // elements must have matching type.
 func Make[T any](v ...T) []T {
 	return v
